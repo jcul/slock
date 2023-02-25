f145f49141db9143813548c2e4d930bb981e87f6
Jack Culhane <jack@databento.com>
2023-02-25 12:11:10 +0000

    Apply slock-mediakeys-20170111 patch

--- slock.c
+++ slock.c
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <X11/extensions/Xrandr.h>
 #include <X11/keysym.h>
+#include <X11/XF86keysym.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <security/pam_appl.h>
@@ -196,6 +197,18 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
 			    IsPrivateKeypadKey(ksym))
 				continue;
 			switch (ksym) {
+			case XF86XK_AudioPlay:
+			case XF86XK_AudioStop:
+			case XF86XK_AudioPrev:
+			case XF86XK_AudioNext:
+			case XF86XK_AudioRaiseVolume:
+			case XF86XK_AudioLowerVolume:
+			case XF86XK_AudioMute:
+			case XF86XK_AudioMicMute:
+			case XF86XK_MonBrightnessDown:
+			case XF86XK_MonBrightnessUp:
+				XSendEvent(dpy, DefaultRootWindow(dpy), True, KeyPressMask, &ev);
+				break;
 			case XK_Return:
 				passwd[len] = '\0';
 				errno = 0;
